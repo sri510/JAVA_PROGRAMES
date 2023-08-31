56f8676a7361bc8ee1cfc62e7f550d29d693c072
Sripriya Sharma <139264751+sri510@users.noreply.github.com>
2023-08-31 23:29:16 +0530

    Create java4.java

---
+++ java4.java
@@ -0,0 +1,19 @@
+/*Write a java program to find Largest Number.
+Input:-
+a=10 b=20
+Output:-
+20 is largerst.
+*/
+package aug30;
+
+public class largest_num {
+	public static void main(String[]args){
+		int a=10,b=20;
+		if(a<b)
+		System.out.println("b="+b);
+		else
+		System.out.println("a="+a);	
+			
+		
+	}
+}
