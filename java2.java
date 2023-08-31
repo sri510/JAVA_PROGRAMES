9d6055fce7842fa7891bb4e1073416c7f8991bf9
Sripriya Sharma <139264751+sri510@users.noreply.github.com>
2023-08-31 23:27:47 +0530

    Create java2.java

---
+++ java2.java
@@ -0,0 +1,20 @@
+/*
+Write a java program to Swap Two Numbers.
+Input:-
+a=5 b=6
+Output:-
+a=6 b=5
+*/
+package aug30;
+
+import java.util.Scanner;
+
+public class swap_1 {
+public static void main(String[]args){
+int a=5,b=6,c;
+c=a;
+a=b;
+b=c;
+System.out.println("a="+a+" b="+b);
+}
+}
