ae716b109d338a72005fe21bd9e153d29927f706
Sripriya Sharma <139264751+sri510@users.noreply.github.com>
2023-08-31 23:26:34 +0530

    Create java1.java

---
+++ java1.java
@@ -0,0 +1,16 @@
+/*
+Write a java program to add two numbers.
+Input:-
+a=5 b=6
+Output:-
+sum=11
+*/
+package aug30;
+
+public class day_1 {
+public static void main(String[]args) {
+	int a=5, b=6;
+	int c=a+b;
+	System.out.println(c);
+}
+}
