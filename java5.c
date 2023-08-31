9cc93cd9d31d5c686d0451552811cb296ca9f439
Sripriya Sharma <139264751+sri510@users.noreply.github.com>
2023-08-31 23:30:09 +0530

    Create java5.c

---
+++ java5.c
@@ -0,0 +1,16 @@
+/*
+Write a java program to Calculate Average of three numbers.
+Input :-
+a=10 b=20 c=30
+Output:-
+Average=20
+*/
+package aug30;
+
+public class avg_ageofthree {
+	public static void main(String[]args) {
+		int a=10, b=20,c=30,x;
+		x=(a+b+c)/3;
+		System.out.println(x);
+}
+}
