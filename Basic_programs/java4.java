83cd7b8bc8c26d7db86183dce6a37780746c06ce
Sripriya Sharma <139264751+sri510@users.noreply.github.com>
2023-09-12 22:13:52 +0530

    Create java4.java

---
+++ Basic_programs/java4.java
@@ -0,0 +1,22 @@
+/*
+write a java program to find the ASCII value of a character
+
+For Example:
+
+Output should be in below format
+
+The ASCII value of a is :97
+*/
+import java.util.*;
+public class AsciiValue {
+    public static void main(String[] args) {
+        
+        char ch='a';
+        int x=(char) ch;
+        
+        //Define variable and a character whose ASCII value you want to find
+        System.out.println("The ASCII value of a is:"+x);
+        // perform th
+
+    }
+}
