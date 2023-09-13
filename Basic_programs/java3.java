108322ba5ddb5783998224be0af3b74b7d1fa7a2
Sripriya Sharma <139264751+sri510@users.noreply.github.com>
2023-09-13 22:26:23 +0530

    java3.java

--- Basic_programs/java3.java
+++ Basic_programs/java3.java
@@ -1,9 +1,10 @@
 /*
 A whole-seller gives incentives in the form of discounts on the net bill. Write a program to input the net bill and calculate the discount as follows:
-   Net Bill in Rupees			  Discount %
-	<5000										5
-	>=5000 and <10000				10
-	>=10000									15
+   Net Bill in Rupees            Discount % 
+       <5000                            5
+       >=5000 and <10000        	10
+       >=10000				15
+       
 Display Discount %, Discount Amount and Amount Payable
 Output:
 Enter Net Bill 8000
