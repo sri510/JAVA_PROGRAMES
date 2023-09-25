377348e56cf32b002ee5334871448f685ab23e51
Sripriya Sharma <139264751+sri510@users.noreply.github.com>
2023-09-25 21:41:52 +0530

    Create java1.java

---
+++ Control_Statement/java1.java
@@ -0,0 +1,19 @@
+/*
+j = 0
+*/
+package Sep_20;
+
+public class MyProgram {
+	
+    public static void main(String [] args) 
+    {
+    	int i = 1, j = -1; 
+    	switch (i) 
+    	{
+    	    case 0, 1: j = 1; /* Line 4 */
+    	    case 2: j = 2; 
+    	    default: j = 0; 
+    	} 
+    	System.out.println("j = " + j);
+    }
+}
