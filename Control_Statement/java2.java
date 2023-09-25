298baacf9445fb2d481959b7961f18ee9ccc6a24
Sripriya Sharma <139264751+sri510@users.noreply.github.com>
2023-09-25 21:42:37 +0530

    Create java2.java

---
+++ Control_Statement/java2.java
@@ -0,0 +1,21 @@
+/*i = 5 and j = 6
+  */
+package Sep_20;
+
+public class MyProgram 
+{	
+    public static void main(String [] args) 
+    {
+    	int i = 1, j = 10; 
+    	do 
+    	{
+    	    if(i > j) 
+    	    {
+    	        break; 
+    	    } 
+    	    j--; 
+    	} 
+    	while (++i < 5); 
+    	System.out.println("i = " + i + " and j = " + j);
+    	}
+}
