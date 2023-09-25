e48ac40c656f498d53fe5ab36c237fab364cdd45
Sripriya Sharma <139264751+sri510@users.noreply.github.com>
2023-09-25 21:48:32 +0530

    Create java7.java

---
+++ Control_Statement/java7.java
@@ -0,0 +1,27 @@
+/*
+1
+*/
+package Sep_20;
+
+public class MyProgram 
+{
+	public static void main(String [] args) 
+    {
+	int I = 0;
+    outer:
+    while (true) 
+    {
+        I++;
+        inner:
+        for (int j = 0; j < 10; j++) 
+        {
+            I += j;
+            if (j == 3)
+                continue inner;
+            break outer;
+        }
+        continue outer;
+    }
+System.out.println(I);
+   }
+}
